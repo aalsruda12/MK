5e508f7eac4903c3c7c54a3032f2c87d82ec08b3
minkyung <aalsruda12@gamil.com>
2025-08-25 17:12:28 +0900

    8/25 MK

---
+++ 01기초문법/ex03.js
@@ -0,0 +1,16 @@
+//대소문자를 전부 구분함
+var name = "홍길동";
+var Name = "이순신";
+
+
+console.log(name, Name);
+
+//붙어있는 단어의 첫  글자는 대문자로 사용합니다.
+//이름에 띄어쓰기는 들어갈 수 없음
+var phoneNumber = 1234; 
+
+//키워드는 이름으로 쓸 수 없음.
+//ex) 
+//var class = 1;
+//var var = 1;
+//var if = 1;
